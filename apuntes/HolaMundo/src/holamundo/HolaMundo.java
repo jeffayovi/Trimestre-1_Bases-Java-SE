19b9eec76f877075846ec9de6acc046e4a37d17e
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-04-16 11:29:06 -0500

    primer hola mundo

---
+++ apuntes/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+
+package holamundo;
+
+/**
+ *
+ * @author Jefferson Ayovi
+ */
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Bienvenido a Java");
+            System.out.println("");
+        }
+    }
+    
+}
