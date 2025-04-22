0a086596e9565addbfa9c86bc4c06d62473518b9
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-04-22 08:25:30 -0500

    Mi PRIMER hOLA mundo

---
+++ ejercicios/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+/*Escribir, compilar y ejecutar el primer programa "Hola Mundo".*/
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
