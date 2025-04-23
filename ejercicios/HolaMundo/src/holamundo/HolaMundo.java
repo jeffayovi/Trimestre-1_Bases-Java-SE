fbdf2e18e7a3e403d9cf9fd2396794179098a2e3
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-04-23 09:08:48 -0500

    Modificar el "Hello World" para incluir variables (int, double, String).

--- ejercicios/HolaMundo/src/holamundo/HolaMundo.java
+++ ejercicios/HolaMundo/src/holamundo/HolaMundo.java
@@ -7,6 +7,15 @@ public class HolaMundo {
     
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        
+        //Modificar el "Hello World" para incluir variables (int, double, String).
+        String nombre = "Jefferson";
+        int edad = 30;
+        double altura = 1.91;
+        
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Edad: "+ edad);
+        System.out.println("Altura: "+ altura);
     }
     
 }
