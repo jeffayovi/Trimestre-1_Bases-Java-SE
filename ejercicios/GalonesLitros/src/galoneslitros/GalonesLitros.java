0ac84cf81f27eb9188f32adcad62759fb1fd4fed
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-04-24 16:19:23 -0500

    Resolver "Try This 1-2" (mejorar conversor de galones a litros con loops).

--- ejercicios/GalonesLitros/src/galoneslitros/GalonesLitros.java
+++ ejercicios/GalonesLitros/src/galoneslitros/GalonesLitros.java
@@ -1,24 +1,37 @@
 
 package galoneslitros;
 /*Conversión de Galones a Litros
-En este proyecto, crearemos un programa que convierta galones a litros. litros en un galón. Así, para convertir galones a
-litros, el valor del galón se multiplica por 3.7854. El programa muestra tanto el número de galones como el número 
-equivalente de litros.
-El programa funcionará declarando dos variables dobles. Una contendrá el número de galones, y la segunda contendrá el número
-de litros después de la conversión. Hay 3.7854 
+Puede utilizar el bucle for, la sentencia if y bloques de código para crear una versión mejorada del conversor de galones
+a litros que desarrolló en el primer proyecto. 
+Esta nueva versión imprimirá una tabla de conversiones, comenzando con 1 galón y terminando en 100 galones.
+Después de cada 10 galones, se imprimirá una línea en blanco. Esto se logra mediante el uso de una variable llamada 
+contador que cuenta el número de líneas que han sido impresas. Preste especial atención a su uso.
+
 */
 
 public class GalonesLitros {
 
     public static void main(String[] args) {
-        double galones;
-        double litros;
+        final double GALONES_A_LITROS = 3.7854;
+        int contador = 0;
         
-        galones = 10;
+        System.out.println("Tabla de Cenvercion de  Galones a Litros");
+        System.out.println("----------------------------------------");
+       
+        for (double galones = 1; galones < 100; galones++) {
+            
+            double litros = galones * GALONES_A_LITROS;
+            System.out.println(galones + " galones es "+ litros +" litros");
+            
+            contador++;
+            
+            //cada 10 linea se imprime una linea negra
+            if (contador == 10) {
+                System.out.println();
+                contador = 0;//reiniciar el contador de linea   
+            } 
+        }
         
-        litros = galones * 3.7854;
-        
-        System.out.println(galones + " galones es  "+ litros + " litros");
     }
     
 }
