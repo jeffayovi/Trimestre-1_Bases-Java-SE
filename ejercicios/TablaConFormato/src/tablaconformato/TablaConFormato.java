7e1d8235ddfa377117f6664cdc4d1fcbc3c6ed82
Jefferson Ayovi <105981230+jeffayovi@users.noreply.github.com>
2025-05-01 10:18:19 -0500

    Practica: Programa que imprima una tabla con saltos de línea (\n) y tabulaciones (\t).

---
+++ ejercicios/TablaConFormato/src/tablaconformato/TablaConFormato.java
@@ -0,0 +1,20 @@
+
+package tablaconformato;
+/*
+Programa que imprima una tabla con saltos de línea (\n) y tabulaciones (\t).
+*/
+
+public class TablaConFormato {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Nombre\t\tEdad\t\tCuidad\t\tPuesto");
+        System.out.println("---------------------------------------------------------------");
+        System.out.println("Ana Garcia\t 25\t\tMadrid\t\tDesarrolladora");
+        System.out.println("Luis Perez\t 30\t\tBarcelona\tDiseñador");
+        System.out.println("Carlos Ruiz\t 35\t\tSevilla\t\tAnalista");
+        System.out.println("Martha Lopez\t 28\t\tValencia\tGerente");
+        
+    }
+    
+}
